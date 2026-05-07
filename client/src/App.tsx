c775a69a8666536419ac0b5a5325e70218f71591
caty21 <94124454+caty21@users.noreply.github.com>
2026-05-07 19:50:11 +0000

    fix: flash blanc supprimé

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,7 +27,7 @@ setTimeout(() => {
 
 function Router() {
   return (
-    <Suspense fallback={null}>
+    <Suspense fallback={<div className="min-h-screen bg-background" />}>
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/about" component={About} />
